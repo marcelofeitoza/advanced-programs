3687aa4a07f21d13c3dbdb65de16bb2aac747a41
marcelofeitoza <feitoza.marcelo7@gmail.com>
2024-11-05 00:48:32 -0300

    feat: enhance contribute instruction with logging and validation improvements

--- fundraiser/src/instructions/contribute.rs
+++ fundraiser/src/instructions/contribute.rs
@@ -1,65 +1,66 @@
-use pinocchio::{
-    account_info::AccountInfo,
-    program_error::ProgramError,
-    pubkey::Pubkey,
-    sysvars::clock::Clock,
-    ProgramResult,
-};
-use pinocchio::sysvars::Sysvar;
 use crate::constants::{MAX_CONTRIBUTION_PERCENTAGE, PERCENTAGE_SCALER, SECONDS_TO_DAYS};
 use crate::errors::FundraiserError;
-use crate::state::{Fundraiser, Contributor};
+use crate::state::Fundraiser;
+use pinocchio::{
+    account_info::AccountInfo, program_error::ProgramError, sysvars::clock::Clock,
+    sysvars::Sysvar, ProgramResult,
+};
+use pinocchio_token::instructions::Transfer;
 
 pub fn process_contribute_instruction(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
-    let [contributor, mint_to_raise, fundraiser, contributor_account, contributor_ata, vault, token_program] = accounts else {
+    let [contributor, mint_to_raise, fundraiser, contributor_account, contributor_ata, vault] =
+        accounts
+    else {
         return Err(ProgramError::NotEnoughAccountKeys);
     };
 
     if data.len() < 8 {
         return Err(ProgramError::InvalidInstructionData);
     }
+
     let amount = unsafe { *(data.as_ptr() as *const u64) };
+    let fundraiser_data = Fundraiser::from_account_info(fundraiser)?;
+    let min_contribution = 1_u64;
+    let max_contribution =
+        (fundraiser_data.amount_to_raise() * MAX_CONTRIBUTION_PERCENTAGE) / PERCENTAGE_SCALER;
+    let current_time = unsafe { Clock::get()?.unix_timestamp };
+    let fundraiser_end_time =
+        fundraiser_data.time_started() + fundraiser_data.duration() as i64 * SECONDS_TO_DAYS;
 
     if !contributor.is_signer() {
         return Err(ProgramError::MissingRequiredSignature);
     }
-
-    let fundraiser_data = Fundraiser::from_account_info(fundraiser)?;
-
     if fundraiser_data.mint_to_raise() != *mint_to_raise.key() {
         return Err(ProgramError::InvalidAccountData);
     }
-
-    let min_contribution = 1_u64;
     if amount < min_contribution {
-        return Err(ProgramError::Custom(FundraiserError::ContributionTooSmall as u32));
+        return Err(ProgramError::Custom(
+            FundraiserError::ContributionTooSmall as u32,
+        ));
     }
-    let max_contribution = (fundraiser_data.amount_to_raise() * MAX_CONTRIBUTION_PERCENTAGE) / PERCENTAGE_SCALER;
     if amount > max_contribution {
-        return Err(ProgramError::Custom(FundraiserError::ContributionTooBig as u32));
+        return Err(ProgramError::Custom(
+            FundraiserError::ContributionTooBig as u32,
+        ));
     }
-
-    let clock = unsafe { *(Clock::get().borrow().as_ptr() as *const Clock) };
-    let current_time = clock.unix_timestamp;
-    let fundraiser_end_time = fundraiser_data.time_started() + fundraiser_data.duration() as i64 * SECONDS_TO_DAYS;
     if current_time > fundraiser_end_time {
-        return Err(ProgramError::Custom(FundraiserError::FundraiserEnded as u32));
+        return Err(ProgramError::Custom(
+            FundraiserError::FundraiserEnded as u32,
+        ));
     }
 
-    let cpi_accounts = spl_token::instruction::Transfer {
-        from: contributor_ata.clone(),
-        to: vault.clone(),
-        authority: contributor.clone(),
-    };
-    let cpi_program = token_program.clone();
+    Transfer {
+        from: contributor_ata,
+        to: vault,
+        authority: contributor,
+        amount,
+    }
+    .invoke()?;
 
-    let seeds = &[b"fundraiser", fundraiser.key().as_ref(), &[fundraiser_data.bump()]];
-    let signer_seeds = &[&seeds[..]];
-    let cpi_context = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
-    transfer(cpi_context)?;
-
-    fundraiser_data.current_amount() += amount;
-    contributor_account.amount() += amount;
+    unsafe {
+        *(fundraiser.borrow_mut_data_unchecked().as_mut_ptr().add(72) as *mut u64) += amount;
+        *(contributor_account.borrow_mut_data_unchecked().as_mut_ptr() as *mut u64) += amount;
+    }
 
     Ok(())
 }
--- fundraiser/src/tests/mod.rs
+++ fundraiser/src/tests/mod.rs
@@ -1,23 +1,20 @@
 #[cfg(test)]
 mod tests_module {
+    use crate::state::Fundraiser;
     use mollusk_svm::{program, Mollusk};
     use solana_sdk::{
         account::AccountSharedData,
         instruction::{AccountMeta, Instruction},
         pubkey::Pubkey,
     };
-    use crate::state::Fundraiser;
 
     #[test]
     fn initialize() {
         let program_id = Pubkey::new_from_array(five8_const::decode_32_const(
-            "22222222222222222222222222222222222222222222"
+            "22222222222222222222222222222222222222222222",
         ));
 
-        let mollusk = Mollusk::new(
-            &program_id,
-            "../target/deploy/fundraiser",
-        );
+        let mollusk = Mollusk::new(&program_id, "../target/deploy/fundraiser");
 
         let maker = Pubkey::new_unique();
         let fundraiser = Pubkey::new_unique();
@@ -32,11 +29,12 @@ mod tests_module {
 
         let data = [
             vec![0],
-            mint.to_bytes().to_vec(), // mint_to_raise
-            100_000_000u64.to_le_bytes().to_vec(), // amount_to_raise
-            slot.to_le_bytes().to_vec(), // time_started
-            1u8.to_le_bytes().to_vec() // duration
-        ].concat();
+            mint.to_bytes().to_vec(),
+            100_000_000u64.to_le_bytes().to_vec(),
+            slot.to_le_bytes().to_vec(),
+            1u8.to_le_bytes().to_vec(),
+        ]
+        .concat();
 
         let instruction = Instruction::new_with_bytes(
             program_id,
@@ -44,7 +42,7 @@ mod tests_module {
             vec![
                 AccountMeta::new(maker, true),
                 AccountMeta::new(fundraiser, false),
-            ]
+            ],
         );
 
         let lamports = mollusk.sysvars.rent.minimum_balance(90);
@@ -56,11 +54,101 @@ mod tests_module {
                     maker,
                     AccountSharedData::new(1_000_000_000, 0, &Pubkey::default()),
                 ),
-                (fundraiser, AccountSharedData::new(lamports, Fundraiser::LEN, &program_id)),
+                (
+                    fundraiser,
+                    AccountSharedData::new(lamports, Fundraiser::LEN, &program_id),
+                ),
                 (system_program, system_program_account),
             ],
         );
 
-        assert!(!result.program_result.is_err());;
+        assert!(!result.program_result.is_err());
+    }
+
+    #[test]
+    fn contribute() {
+        // Setup and fundraiser initialization
+        let program_id = Pubkey::new_from_array(five8_const::decode_32_const(
+            "22222222222222222222222222222222222222222222",
+        ));
+
+        let mollusk = Mollusk::new(&program_id, "../target/deploy/fundraiser");
+
+        let maker = Pubkey::new_unique();
+        let fundraiser = Pubkey::new_unique();
+        let mint = Pubkey::new_unique();
+
+        let slot = mollusk.sysvars.clock.slot + 200;
+
+        let (system_program, system_program_account) = program::keyed_account_for_system_program();
+
+        let (fundraiser_pda, bump) =
+            Pubkey::try_find_program_address(&[fundraiser.as_ref()], &program_id).unwrap();
+
+        let data = [
+            vec![0],
+            mint.to_bytes().to_vec(),
+            100_000_000u64.to_le_bytes().to_vec(),
+            slot.to_le_bytes().to_vec(),
+            1u8.to_le_bytes().to_vec(),
+        ]
+        .concat();
+
+        let instruction = Instruction::new_with_bytes(
+            program_id,
+            &data,
+            vec![
+                AccountMeta::new(maker, true),
+                AccountMeta::new(fundraiser, false),
+            ],
+        );
+
+        let lamports = mollusk.sysvars.rent.minimum_balance(90);
+
+        let result: mollusk_svm::result::InstructionResult = mollusk.process_instruction(
+            &instruction,
+            &vec![
+                (
+                    maker,
+                    AccountSharedData::new(1_000_000_000, 0, &Pubkey::default()),
+                ),
+                (
+                    fundraiser,
+                    AccountSharedData::new(lamports, Fundraiser::LEN, &program_id),
+                ),
+                (system_program.clone(), system_program_account.clone()),
+            ],
+        );
+
+        assert!(!result.program_result.is_err());
+
+        // Contribution
+        let data = [vec![1], 1000u64.to_le_bytes().to_vec()].concat();
+
+        let instruction = Instruction::new_with_bytes(
+            program_id,
+            &data,
+            vec![
+                AccountMeta::new(maker, true),
+                AccountMeta::new(fundraiser, false),
+            ],
+        );
+
+        let result: mollusk_svm::result::InstructionResult = mollusk.process_instruction(
+            &instruction,
+            &vec![
+                (
+                    maker,
+                    AccountSharedData::new(1_000_000_000, 0, &Pubkey::default()),
+                ),
+                (
+                    fundraiser,
+                    AccountSharedData::new(lamports, Fundraiser::LEN, &program_id),
+                ),
+                (system_program, system_program_account),
+            ],
+        );
+
+        assert!(!result.program_result.is_err());
     }
 }
