2269656b1d227fe393b836af3a77a9124632f295
marcelofeitoza <feitoza.marcelo7@gmail.com>
2024-11-08 07:50:32 -0300

    refactor: streamline refund instruction processing by removing unused maker variable and adjusting account handling

--- fundraiser/src/instructions/refund.rs
+++ fundraiser/src/instructions/refund.rs
@@ -1,30 +1,30 @@
+use crate::state::{Contributor, Fundraiser};
 use pinocchio::account_info::AccountInfo;
 use pinocchio::program_error::ProgramError;
-use pinocchio::{signer, ProgramResult};
+use pinocchio::{msg, signer, ProgramResult};
 use pinocchio_token::instructions::Transfer;
-use crate::state::{Contributor, Fundraiser};
+use pinocchio_token::state::TokenAccount;
 
 pub fn process_refund_instruction(accounts: &[AccountInfo]) -> ProgramResult {
-    let [maker, fundraiser, contributor_account, contributor_ta, vault, token_program] =
-        accounts
-    else {
+    let [fundraiser, contributor_account, contributor_ta, vault, token_program] = accounts else {
         return Err(ProgramError::NotEnoughAccountKeys);
     };
 
     let fundraiser_account = Fundraiser::from_account_info(fundraiser)?;
     let contributor_account = Contributor::from_account_info(contributor_account)?;
+    let maker = fundraiser_account.maker();
     let bump = fundraiser_account.bump();
     let fundraiser_seed = b"fundraiser".as_ref();
-    let signer_key_seed = maker.key().as_ref();
+    let signer_key_seed = maker.as_ref();
     let bump_seed = &[bump];
+
     Transfer {
         from: vault,
         to: contributor_ta,
         authority: fundraiser,
         amount: contributor_account.amount(),
     }
-        .invoke_signed(&[signer!(fundraiser_seed, signer_key_seed, bump_seed)])?;
-
+    .invoke_signed(&[signer!(fundraiser_seed, signer_key_seed, bump_seed)])?;
 
     Ok(())
 }
