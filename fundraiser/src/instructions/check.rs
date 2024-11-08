dd77b5d078b33ebf43d3169ecfe9208fcb8d5409
marcelofeitoza <feitoza.marcelo7@gmail.com>
2024-11-07 22:34:12 -0300

    feat: simplify check instruction processing by removing unused code and enhancing transfer logic

--- fundraiser/src/instructions/check.rs
+++ fundraiser/src/instructions/check.rs
@@ -1,92 +1,33 @@
-use pinocchio::account_info::AccountInfo;
-use pinocchio::ProgramResult;
+use crate::state::Fundraiser;
+use pinocchio::{account_info::AccountInfo, program_error::ProgramError, signer, ProgramResult};
+use pinocchio_token::instructions::Transfer;
+use pinocchio_token::state::TokenAccount;
 
-pub fn process_check_contributions_instruction(
-    _accounts: &[AccountInfo],
-    _data: &[u8],
-) -> ProgramResult {
-    // use anchor_lang::prelude::*;
-    // use anchor_spl::{
-    //     associated_token::AssociatedToken,
-    //     token::{
-    //         transfer,
-    //         Mint,
-    //         Token,
-    //         TokenAccount,
-    //         Transfer
-    //     }
-    // };
+pub fn process_check_instruction(accounts: &[AccountInfo]) -> ProgramResult {
+    let [signer, signer_ta, fundraiser, vault, token_program] = accounts else {
+        return Err(ProgramError::NotEnoughAccountKeys);
+    };
+
+    let fundraiser_account = Fundraiser::from_account_info(fundraiser)?;
+    // let vault_ta = TokenAccount::from_account_info(vault)?;
     //
-    // use crate::{
-    //     state::Fundraiser,
-    //     FundraiserError
-    // };
+    // assert!(
+    //     vault_ta.amount() >= fundraiser_account.amount_to_raise(),
+    //     "Fundraiser not met"
+    // );
     //
-    // #[derive(Accounts)]
-    // pub struct CheckContributions<'info> {
-    //     #[account(mut)]
-    //     pub maker: Signer<'info>,
-    //     pub mint_to_raise: Account<'info, Mint>,
-    //     #[account(
-    //     mut,
-    //     seeds = [b"fundraiser".as_ref(), maker.key().as_ref()],
-    //     bump = fundraiser.bump,
-    //     close = maker,
-    //     )]
-    //     pub fundraiser: Account<'info, Fundraiser>,
-    //     #[account(
-    //     mut,
-    //     associated_token::mint = mint_to_raise,
-    //     associated_token::authority = fundraiser,
-    //     )]
-    //     pub vault: Account<'info, TokenAccount>,
-    //     #[account(
-    //     init_if_needed,
-    //     payer = maker,
-    //     associated_token::mint = mint_to_raise,
-    //     associated_token::authority = maker,
-    //     )]
-    //     pub maker_ata: Account<'info, TokenAccount>,
-    //     pub token_program: Program<'info, Token>,
-    //     pub system_program: Program<'info, System>,
-    //     pub associated_token_program: Program<'info, AssociatedToken>,
-    // }
-    //
-    // impl<'info> CheckContributions<'info> {
-    //     pub fn check_contributions(&self) -> Result<()> {
-    //
-    //         // Check if the target amount has been met
-    //         require!(
-    //         self.vault.amount >= self.fundraiser.amount_to_raise,
-    //         FundraiserError::TargetNotMet
-    //     );
-    //
-    //         // Transfer the funds to the maker
-    //         // CPI to the token program to transfer the funds
-    //         let cpi_program = self.token_program.to_account_info();
-    //
-    //         // Transfer the funds from the vault to the maker
-    //         let cpi_accounts = Transfer {
-    //             from: self.vault.to_account_info(),
-    //             to: self.maker_ata.to_account_info(),
-    //             authority: self.fundraiser.to_account_info(),
-    //         };
-    //
-    //         // Signer seeds to sign the CPI on behalf of the fundraiser account
-    //         let signer_seeds: [&[&[u8]]; 1] = [&[
-    //             b"fundraiser".as_ref(),
-    //             self.maker.to_account_info().key.as_ref(),
-    //             &[self.fundraiser.bump],
-    //         ]];
-    //
-    //         // CPI context with signer since the fundraiser account is a PDA
-    //         let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, &signer_seeds);
-    //
-    //         // Transfer the funds from the vault to the maker
-    //         transfer(cpi_ctx, self.vault.amount)?;
-    //
-    //         Ok(())
-    //     }
-    // }
+    let bump = fundraiser_account.bump();
+    let fundraiser_seed = b"fundraiser".as_ref();
+    let signer_key_seed = signer.key().as_ref();
+    let bump_seed = &[bump];
+
+    Transfer {
+        from: vault,
+        to: signer_ta,
+        authority: fundraiser,
+        amount: fundraiser_account.current_amount(),
+    }
+    .invoke_signed(&[signer!(fundraiser_seed, signer_key_seed, bump_seed)])?;
+
     Ok(())
 }
---
+++ fundraiser/src/tests/check_test.rs
@@ -0,0 +1,115 @@
+use crate::state::Fundraiser;
+use crate::tests::{setup, utils};
+use pinocchio_token::state::TokenAccount;
+use solana_sdk::account::{AccountSharedData, ReadableAccount};
+use solana_sdk::{
+    instruction::{AccountMeta, Instruction},
+    program_pack::Pack,
+    pubkey::Pubkey,
+};
+
+#[test]
+fn check_test() {
+    let (program_id, mut mollusk) = setup();
+    let (token_program, token_program_account) = mollusk_token::token::keyed_account();
+
+    let maker = Pubkey::new_unique();
+    let signer = maker;
+    let signer_ta = Pubkey::new_unique();
+    let fundraiser =
+        Pubkey::find_program_address(&[b"fundraiser", &maker.to_bytes()], &program_id).0;
+    let mint = Pubkey::new_unique();
+    let vault = Pubkey::new_unique();
+    let mint_account = utils::pack_mint(&signer, 1_000_000);
+    let signer_ta_account = utils::pack_token_account(&signer, &mint, 1_000_000);
+    let vault_account = utils::pack_token_account(&fundraiser, &mint, 0);
+
+    let mut fundraiser_account = AccountSharedData::new(
+        mollusk.sysvars.rent.minimum_balance(Fundraiser::LEN),
+        Fundraiser::LEN,
+        &program_id,
+    );
+
+    let amount_to_raise: u64 = 100;
+    let current_amount: u64 = 0;
+    let time_started = 200i64;
+    let duration = 1u8;
+    let bump = 1u8;
+
+    fundraiser_account.set_data_from_slice(
+        &[
+            maker.to_bytes().to_vec(),
+            mint.to_bytes().to_vec(),
+            amount_to_raise.to_le_bytes().to_vec(),
+            current_amount.to_le_bytes().to_vec(),
+            time_started.to_le_bytes().to_vec(),
+            duration.to_le_bytes().to_vec(),
+            bump.to_le_bytes().to_vec(),
+        ]
+        .concat(),
+    );
+
+    let data = &[vec![2]].concat();
+
+    let check_instruction = Instruction::new_with_bytes(
+        program_id,
+        data,
+        vec![
+            AccountMeta::new(signer, true),
+            AccountMeta::new(signer_ta, false),
+            AccountMeta::new(fundraiser, true),
+            AccountMeta::new(vault, true),
+            AccountMeta::new_readonly(token_program, false),
+        ],
+    );
+
+
+    let result = mollusk.process_instruction(
+        &check_instruction,
+        &vec![
+            (signer, AccountSharedData::new(1_000_000, 0, &program_id)),
+            (signer_ta, signer_ta_account),
+            (fundraiser, fundraiser_account.clone()),
+            (vault, vault_account.clone()),
+            (token_program, token_program_account),
+        ],
+    );
+    assert!(
+        !result.program_result.is_err(),
+        "process_check_instruction failed."
+    );
+
+    let fundraiser_result_account = result
+        .get_account(&fundraiser)
+        .expect("Failed to find fundraiser account");
+    let data = fundraiser_result_account.data();
+    println!("Fundraiser data:");
+    println!(
+        "Maker: {:?}",
+        Pubkey::new_from_array(data[0..32].try_into().unwrap())
+    );
+    println!(
+        "Mint to raise: {:?}",
+        Pubkey::new_from_array(data[32..64].try_into().unwrap())
+    );
+    println!(
+        "Amount to raise: {:?}",
+        u64::from_le_bytes(data[64..72].try_into().unwrap())
+    );
+    println!(
+        "Current amount: {:?}",
+        u64::from_le_bytes(data[72..80].try_into().unwrap())
+    );
+    println!(
+        "Time started: {:?}",
+        i64::from_le_bytes(data[80..88].try_into().unwrap())
+    );
+    println!(
+        "Duration: {:?}",
+        u8::from_le_bytes(data[88..89].try_into().unwrap())
+    );
+    println!(
+        "Bump seed: {:?}",
+        u8::from_le_bytes(data[89..90].try_into().unwrap())
+    );
+}
