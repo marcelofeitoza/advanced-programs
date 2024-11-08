6f2272817a11fa98f527d86e31b65fd05dc05e58
marcelofeitoza <feitoza.marcelo7@gmail.com>
2024-11-07 22:33:39 -0300

    feat: enhance contribution processing with improved validation and streamlined logic

--- fundraiser/src/instructions/contribute.rs
+++ fundraiser/src/instructions/contribute.rs
@@ -1,39 +1,43 @@
-use crate::state::Fundraiser;
-use pinocchio::{account_info::AccountInfo, msg, program_error::ProgramError, ProgramResult};
+use crate::constants::MIN_AMOUNT_TO_RAISE;
+use pinocchio::{account_info::AccountInfo, program_error::ProgramError, ProgramResult};
 use pinocchio_token::instructions::Transfer;
 
 pub fn process_contribute_instruction(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
-    let [contributor, _mint_to_raise, fundraiser, contributor_ata, contributor_account, vault, _token_program] =
-        accounts
-    else {
+    let amount: u64 = unsafe { *(data.as_ptr() as *const u64) };
+    assert!(amount >= MIN_AMOUNT_TO_RAISE, "Amount too low"); // 2 CUs
+
+    let [signer, contributor, signer_ta, fundraiser, mint, vault, _token_program] = accounts else {
         return Err(ProgramError::NotEnoughAccountKeys);
     };
 
-    let amount = u64::from_le_bytes(data[0..8].try_into().unwrap());
-    msg!("Amount from data: {}", amount);
+    // let fundraiser_account = Fundraiser::from_account_info_unchecked(
+    //     fundraiser);
+    // let contributor_account = Contributor::from_account_info_unchecked(contributor);
+    // assert_eq!(
+    //     &fundraiser_account.mint_to_raise(),
+    //     mint.key(),
+    //     "Invalid mint"
+    // ); // 28 CUs
+    // assert!(
+    //     fundraiser_account.time_started() > 0,
+    //     "Fundraiser not started yet"
+    // ); // 2 CUs
+    // assert!(
+    //     fundraiser_account.time_started() + i64::from(fundraiser_account.duration()) > 0,
+    //     "Fundraiser ended"
+    // ); // 4 CUs
 
-    // Transfer {
-    //     from: contributor_ata,
-    //     to: vault,
-    //     authority: contributor,
-    //     amount,
-    // }
-    // .invoke()
-    // .map_err(|_| ProgramError::AccountBorrowFailed)?;
+    Transfer {
+        from: signer_ta,
+        to: vault,
+        authority: signer,
+        amount,
+    }
+    .invoke()?; // 5K+ CUs
 
-    let fundraiser_account_data = Fundraiser::from_account_info(fundraiser)?;
     unsafe {
-        fundraiser.borrow_mut_data_unchecked()[72..80]
-            .copy_from_slice(&(fundraiser_account_data.current_amount() + amount).to_le_bytes());
-        msg!(
-            "Fundraiser account current amount: {}",
-            Fundraiser::from_account_info(fundraiser)?.current_amount()
-        );
-
-        *(contributor_account.borrow_mut_data_unchecked().as_mut_ptr() as *mut u64) =
-            u64::from_le_bytes(amount.to_le_bytes());
-
-        msg!("Contributed {} to the fundraiser", amount);
+        *(fundraiser.borrow_mut_data_unchecked().as_mut_ptr().add(72) as *mut u64) += amount;
+        *(contributor.borrow_mut_data_unchecked().as_mut_ptr() as *mut u64) += amount;
     }
 
     Ok(())
